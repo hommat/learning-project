742beb3d3c7790715151daab440f3da92585e781
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2020-11-01 17:21:54 +0100

    Fix tests

--- articles/src/service.test.js
+++ articles/src/service.test.js
@@ -5,7 +5,7 @@ const articleService = new ArticleService();
 describe("ArticleService", () => {
     describe("getBooks", () => {
         test("returns not empty array", () => {
-            expect(articleService.getArticles().length).not.toBe(2);
+            expect(articleService.getArticles().length).not.toBe(0);
         })
     })
 })
